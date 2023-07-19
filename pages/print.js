e57e6609587e47b44373049a85f0169d08e8cf14
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-07-19 14:33:15 -0400

    Styled print page and set up Supabase fetch

--- pages/print.js
+++ pages/print.js
@@ -1,29 +1,51 @@
+import { useEffect, useState } from 'react';
 import styles from '../styles/Print.module.css'
-import createPdf from '../scripts/createPdf'; // Make sure the path points to the correct location
-
-async function passPdfToPrinter(content) {
-  try {
-    const pdfBuffer = await createPdf(content);
-    // Now you can upload the pdfBuffer to your API or use it as you need
-  } catch (error) {
-    // Handle any errors that occurred during PDF generation
-    console.error(error);
-  }
-}
 
 const Print = () => {
-    return (
-        <div className="container">
-            <main className="main">
-                <h1 className={styles.title}>
-                    SessionScribe
-                </h1>
-                <h1 className={styles.title}>
-                    Print a transcript
-                </h1>
-            </main>
+  const [transcripts, setTranscripts] = useState([]);
+
+  useEffect(() => {
+    fetchTranscripts();
+  }, []);
+
+  const fetchTranscripts = async () => {
+    try {
+      const res = await fetch('/api/supabaseFetch');
+      const data = await res.json();
+
+      setTranscripts(data);  // Update the transcripts state
+    } catch (error) {
+      console.error('An error occurred while fetching the transcripts:', error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <main className="main">
+        <h1 className={styles.title}>
+          SessionScribe
+        </h1>
+        <h1 className={styles.title}>
+          Print a transcript
+        </h1>
+        <p className={styles.description}> Print a session transcript and retrieve it from the conference printing station </p>
+
+        {/* Display the transcripts */}
+        <div className={styles.grid}>
+          {transcripts.map((transcript, index) => (
+            <div key={index} className={styles.row}>
+              <div className={styles.cell}>{new Date(transcript.created_at).toLocaleString()}</div>
+              <div className={styles.cell}>{transcript.session_name}</div>
+              <div className={styles.cell}>{transcript.presenters}</div>
+              <div className={styles.cell}>
+                <button className = {styles.printbutton} id={transcript.id} onClick={() => printTranscript(transcript.id)}>Print</button>
+              </div>
+            </div>
+          ))}
         </div>
-    )
-}
+      </main>
+    </div>
+  );
+};
 
 export default Print;
