ee0ab535bef8fd17efe8ea5f27901b07a7fbea23
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2023-07-19 11:57:02 -0400

    Added a Header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,20 +1,23 @@
 // Header.js
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 import styles from '../styles/Header.module.css';
+import Link from 'next/link';
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <nav className={styles.navbar}>
-          <Link href="/">
-            Home
-          </Link>
-          <Link href="/print">
-            Print
-          </Link>
-          <Link href="/record">
-            Record
-          </Link>
+      <Link href="/">
+        <p className={router.pathname === "/" ? styles.activeLink : ""}>Home</p>
+      </Link>
+      <Link href="/print">
+        <p className={router.pathname === "/print" ? styles.activeLink : ""}>Print</p>
+      </Link>
+      <Link href="/record">
+        <p className={router.pathname === "/record" ? styles.activeLink : ""}>Record</p>
+      </Link>
     </nav>
   );
 }
